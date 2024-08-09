64baee5c355256572e3cb1e21f3478413e4a8745
Wajsar Josef <36114753+PepikVaio@users.noreply.github.com>
2024-08-09 23:14:42 +0200

    Update extract.sh
    Pojmenované proměnné, podle mého standadu.

--- extract.sh
+++ extract.sh
@@ -1,18 +1,18 @@
 #!/bin/bash
 
-# Název složky, která začíná na eaf3f838 (nepovinný)
-folder_prefix=""
+# Vzdálený reMarkable_Server
+reMarkable_Path="/home/root/.local/share/remarkable/xochitl/"
+reMarkable_Server="root@10.0.1.24"
+reMarkable_Password=""
 
-# Vzdálený server a cesta
-server="root@10.0.1.24"
-remote_path="/home/root/.local/share/remarkable/xochitl/"
-password=""
+# ID souboru, která začíná na eaf3f838 (nepovinný)
+reMarkable_File_ID=""
 
 # Funkce pro nalezení nejnovějšího souboru
-find_latest_file() {
+find_Latest_File() {
     local search_path="$1"
     local search_pattern="$2"
-    sshpass -p "$password" ssh "$server" "
+    sshpass -p "$reMarkable_Password" ssh "$reMarkable_Server" "
         find \"$search_path\" -type f -name '$search_pattern' -print | while read file; do
             echo \"\$(stat -c '%Y' \"\$file\") \$file\"
         done | sort -n | tail -1 | awk '{print \$2}'
@@ -20,9 +20,9 @@ find_latest_file() {
 }
 
 # Funkce pro nalezení nejnovější složky
-find_latest_directory() {
+find_Latest_Directory() {
     local search_path="$1"
-    sshpass -p "$password" ssh "$server" "
+    sshpass -p "$reMarkable_Password" ssh "$reMarkable_Server" "
         find \"$search_path\" -maxdepth 1 -type d ! -name '*.*' ! -name 'xochitl' -print | while read dir; do
             echo \"\$(stat -c '%Y' \"\$dir\") \$dir\"
         done | sort -n | tail -1 | awk '{print \$2}'
@@ -30,22 +30,22 @@ find_latest_directory() {
 }
 
 # Funkce pro stažení souboru
-download_file() {
+download_File() {
     local file_path="$1"
     echo "Stahuji soubor: $file_path"
-    sshpass -p "$password" scp "$server:\"$file_path\"" .
+    sshpass -p "$reMarkable_Password" scp "$reMarkable_Server:\"$file_path\"" .
 }
 
 # Hlavní část skriptu
-if [ -z "$folder_prefix" ]; then
-    # Pokud je folder_prefix prázdný, najdi nejnovější složku bez tečky v názvu
-    latest_directory=$(find_latest_directory "$remote_path")
+if [ -z "$reMarkable_File_ID" ]; then
+    # Pokud je reMarkable_File_ID prázdný, najdi nejnovější složku bez tečky v názvu
+    latest_directory=$(find_Latest_Directory "$reMarkable_Path")
 
     echo "Nalezená složka: $latest_directory"  # Pro ladění
 
-    if [ -n "$latest_directory" ] && [ "$latest_directory" != "$remote_path" ]; then
+    if [ -n "$latest_directory" ] && [ "$latest_directory" != "$reMarkable_Path" ]; then
         # Nalezení nejnovějšího souboru v nalezené složce
-        latest_file=$(find_latest_file "$latest_directory" "*")
+        latest_file=$(find_Latest_File "$latest_directory" "*")
 
         if [ -n "$latest_file" ]; then
             # Získání názvu souboru bez cesty
@@ -54,7 +54,7 @@ if [ -z "$folder_prefix" ]; then
             echo "Soubor: $file_name"
 
             # Volání funkce pro stažení souboru
-            download_file "$latest_file"
+            download_File "$latest_file"
         else
             echo "Ve složce '$latest_directory' nejsou žádné soubory."
         fi
@@ -62,12 +62,12 @@ if [ -z "$folder_prefix" ]; then
         echo "Složka nebyla nalezena."
     fi
 else
-    # Pokud je folder_prefix neprázdný, vyhledej složku podle prefixu
-    latest_directory=$(find_latest_directory "$remote_path" "${folder_prefix}*")
+    # Pokud je reMarkable_File_ID neprázdný, vyhledej složku podle prefixu
+    latest_directory=$(find_Latest_Directory "$reMarkable_Path" "${reMarkable_File_ID}*")
 
     if [ -n "$latest_directory" ]; then
         # Nalezení nejnovějšího souboru v nalezené složce
-        latest_file=$(find_latest_file "$latest_directory" "*")
+        latest_file=$(find_Latest_File "$latest_directory" "*")
 
         if [ -n "$latest_file" ]; then
             # Získání názvu souboru bez cesty
@@ -76,11 +76,11 @@ else
             echo "Soubor: $file_name"
 
             # Volání funkce pro stažení souboru
-            download_file "$latest_file"
+            download_File "$latest_file"
         else
             echo "Ve složce '$latest_directory' nejsou žádné soubory."
         fi
     else
-        echo "Složka podle prefixu '$folder_prefix' nebyla nalezena."
+        echo "Složka podle prefixu '$reMarkable_File_ID' nebyla nalezena."
     fi
 fi
