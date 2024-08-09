9289bc2c628cb8e117dd47d657eda9e576ce9d8c
Wajsar Josef <36114753+PepikVaio@users.noreply.github.com>
2024-08-09 22:56:55 +0200

    Update extract.sh
    Přidána proměnná pro heslo.

--- extract.sh
+++ extract.sh
@@ -6,17 +6,25 @@ folder_prefix=""
 # Vzdálený server a cesta
 server="root@10.0.1.24"
 remote_path="/home/root/.local/share/remarkable/xochitl/"
+password=""
 
 # Funkce pro nalezení nejnovějšího souboru
 find_latest_file() {
     local search_path="$1"
     local search_pattern="$2"
-    
-    ssh "$server" "
-        find \"$search_path\" -maxdepth 1 -type d -name '$search_pattern' ! -name '*.*' -print | while read dir; do
-            find \"\$dir\" -type f -print | while read file; do
-                echo \"\$(stat -c '%Y' \"\$file\") \$file\"
-            done
+    sshpass -p "$password" ssh "$server" "
+        find \"$search_path\" -type f -name '$search_pattern' -print | while read file; do
+            echo \"\$(stat -c '%Y' \"\$file\") \$file\"
+        done | sort -n | tail -1 | awk '{print \$2}'
+    "
+}
+
+# Funkce pro nalezení nejnovější složky
+find_latest_directory() {
+    local search_path="$1"
+    sshpass -p "$password" ssh "$server" "
+        find \"$search_path\" -maxdepth 1 -type d ! -name '*.*' ! -name 'xochitl' -print | while read dir; do
+            echo \"\$(stat -c '%Y' \"\$dir\") \$dir\"
         done | sort -n | tail -1 | awk '{print \$2}'
     "
 }
@@ -25,17 +33,13 @@ find_latest_file() {
 download_file() {
     local file_path="$1"
     echo "Stahuji soubor: $file_path"
-    scp "$server:\"$file_path\"" .
+    sshpass -p "$password" scp "$server:\"$file_path\"" .
 }
 
 # Hlavní část skriptu
 if [ -z "$folder_prefix" ]; then
     # Pokud je folder_prefix prázdný, najdi nejnovější složku bez tečky v názvu
-    latest_directory=$(ssh "$server" "
-        find \"$remote_path\" -maxdepth 1 -type d ! -name '*.*' ! -name 'xochitl' -print | while read dir; do
-            echo \"\$(stat -c '%Y' \"\$dir\") \$dir\"
-        done | sort -n | tail -1 | awk '{print \$2}'
-    ")
+    latest_directory=$(find_latest_directory "$remote_path")
 
     echo "Nalezená složka: $latest_directory"  # Pro ladění
 
@@ -59,17 +63,24 @@ if [ -z "$folder_prefix" ]; then
     fi
 else
     # Pokud je folder_prefix neprázdný, vyhledej složku podle prefixu
-    latest_file=$(find_latest_file "$remote_path" "${folder_prefix}*")
+    latest_directory=$(find_latest_directory "$remote_path" "${folder_prefix}*")
 
-    if [ -n "$latest_file" ]; then
-        # Získání názvu souboru bez cesty
-        file_name=$(basename "$latest_file")
-        echo "Nejnověji upravený soubor!"
-        echo "Soubor: $file_name"
+    if [ -n "$latest_directory" ]; then
+        # Nalezení nejnovějšího souboru v nalezené složce
+        latest_file=$(find_latest_file "$latest_directory" "*")
 
-        # Volání funkce pro stažení souboru
-        download_file "$latest_file"
+        if [ -n "$latest_file" ]; then
+            # Získání názvu souboru bez cesty
+            file_name=$(basename "$latest_file")
+            echo "Nejnověji upravený soubor!"
+            echo "Soubor: $file_name"
+
+            # Volání funkce pro stažení souboru
+            download_file "$latest_file"
+        else
+            echo "Ve složce '$latest_directory' nejsou žádné soubory."
+        fi
     else
-        echo "Soubor nebyl nalezen."
+        echo "Složka podle prefixu '$folder_prefix' nebyla nalezena."
     fi
 fi
