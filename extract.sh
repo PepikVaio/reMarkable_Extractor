01e9fd7decb1afeea2ec40c47bbb6feb31836e6d
Wajsar Josef <36114753+PepikVaio@users.noreply.github.com>
2024-08-11 01:24:30 +0200

    Update extract.sh

--- extract.sh
+++ extract.sh
@@ -1,7 +1,13 @@
 #!/bin/bash
 
+# Cesta k souborům
+reMarkable_Path="/home/remarkable/.local/share/remarkable/xochitl/"
+
 WGET="wget"
 
+
+# Pro stahování souborů v reMarkable.
+# reMarkable má staré wget, které nepodporuje STL, toto chybu opraví.
 upgrade_WGET () {
     wget_path=/home/root/.local/share/@Wajsar_Josef/wget
     wget_remote=http://toltec-dev.org/thirdparty/bin/wget-v1.21.1-1
@@ -30,4 +36,84 @@ upgrade_WGET () {
     WGET="$wget_path"
 }
 
+# Funkce pro nalezení nejnovějšího souboru
+find_Latest_File() {
+    local search_path="$1"
+    local search_pattern="$2"
+    find "$search_path" -type f -name "$search_pattern" -print | while read -r file; do
+        echo "$(stat -c '%Y' "$file") $file"
+    done | sort -n | tail -1 | awk '{print $2}'
+}
+
+# Funkce pro nalezení nejnovější složky bez tečky na konci
+find_Latest_Directory() {
+    local search_path="$1"
+    local search_pattern="$2"
+    find "$search_path" -maxdepth 1 -type d -name "$search_pattern" ! -name '*.*' ! -name 'xochitl' -print | while read -r dir; do
+        echo "$(stat -c '%Y' "$dir") $dir"
+    done | sort -n | tail -1 | awk '{print $2}'
+}
+
+# Funkce pro stažení souboru
+download_File() {
+    local file_path="$1"
+    echo "Stahuji soubor: $file_path"
+    cp "$file_path" .  # Změněno na cp pro kopírování z reMarkable na lokální systém
+}
+
+# Funkce pro zpracování nalezeného souboru
+process_Latest_File() {
+    local latest_file="$1"
+    local latest_directory="$2"
+
+    if [ -n "$latest_file" ]; then
+        # Získání názvu souboru bez cesty
+        file_name=$(basename "$latest_file")
+        echo "Nejnověji upravený soubor: $file_name"
+
+        # Volání funkce pro stažení souboru
+        download_File "$latest_file"
+    else
+        echo "Ve složce '$latest_directory' nejsou žádné soubory."
+    fi
+}
+
+
+
+
+
+
+
+
+
+
 upgrade_WGET
+
+# Hlavní část skriptu
+if [ -z "$reMarkable_File_ID" ]; then
+    # Pokud je reMarkable_File_ID prázdný, najdi nejnovější složku bez tečky v názvu
+    latest_directory=$(find_Latest_Directory "$reMarkable_Path" "*")
+
+    if [ -n "$latest_directory" ] && [ "$latest_directory" != "$reMarkable_Path" ]; then
+        # Nalezení nejnovějšího souboru v nalezené složce
+        latest_file=$(find_Latest_File "$latest_directory" "*")
+
+        # Volání funkce pro zpracování nalezeného souboru
+        process_Latest_File "$latest_file" "$latest_directory"
+    else
+        echo "Složka nebyla nalezena."
+    fi
+else
+    # Pokud je reMarkable_File_ID neprázdný, vyhledej složku podle prefixu
+    latest_directory=$(find_Latest_Directory "$reMarkable_Path" "${reMarkable_File_ID}*")
+
+    if [ -n "$latest_directory" ]; then
+        # Nalezení nejnovějšího souboru v nalezené složce
+        latest_file=$(find_Latest_File "$latest_directory" "*")
+
+        # Volání funkce pro zpracování nalezeného souboru
+        process_Latest_File "$latest_file" "$latest_directory"
+    else
+        echo "Složka podle prefixu '$reMarkable_File_ID' nebyla nalezena."
+    fi
+fi
