bb7e33da616774efce279d37c9287a3d642b1c31
Wajsar Josef <36114753+PepikVaio@users.noreply.github.com>
2024-08-11 19:32:44 +0200

    Update import.sh

--- import.sh
+++ import.sh
@@ -1,9 +1,5 @@
 #!/bin/bash
 
-
-
-
-
 #    ___      _   _   _                              __  __          _        _    _     
 #   / __| ___| |_| |_(_)_ _  __ _ ___  ___   _ _ ___|  \/  |__ _ _ _| |____ _| |__| |___ 
 #   \__ \/ -_)  _|  _| | ' \/ _` (_-< |___| | '_/ -_) |\/| / _` | '_| / / _` | '_ \ / -_)
@@ -19,36 +15,67 @@ reMarkable_File_ID="${2:-}"
 # Cesta k souborům
 reMarkable_Path="/home/root/.local/share/remarkable/xochitl/"
 
+# Pro stahování souborů v reMarkable.
+# reMarkable má staré wget, které nepodporuje STL, toto chybu opraví.
+upgrade_WGET() {
+    wget_path=/home/root/.local/share/@Wajsar_Josef/wget
+    wget_remote=http://toltec-dev.org/thirdparty/bin/wget-v1.21.1-1
+    wget_checksum=c258140f059d16d24503c62c1fdf747ca843fe4ba8fcd464a6e6bda8c3bbb6b5
 
+    if [ -f "$wget_path" ] && ! echo "$wget_checksum  $wget_path" | sha256sum -c -; then
+        rm "$wget_path"
+    fi
+
+    if [ ! -f "$wget_path" ]; then
+        echo "Info: Načítání zabezpečeného wget"
+        mkdir -p "$(dirname "$wget_path")"
+        if ! wget -cq "$wget_remote" --output-document "$wget_path"; then
+            echo "Error: Nelze načíst wget, ujistěte se, že máte stabilní připojení Wi-Fi"
+            exit 1
+        fi
+    fi
+
+    if ! echo "$wget_checksum  $wget_path" | sha256sum -c -; then
+        echo "Error: Neplatný kontrolní součet pro místní binární soubor wget"
+        exit 1
+    fi
+
+    chmod 755 "$wget_path"
+    WGET="$wget_path"
+}
 
 # Funkce pro nalezení složky, která začíná na reMarkable_File_ID a nemá tečku na konci
-find_Directory() {
+find_Matching_Directory() {
     local search_path="$1"
     local search_pattern="$2"
     find "$search_path" -maxdepth 1 -type d -name "$search_pattern" ! -name '*.*' ! -name 'xochitl' -print -quit
 }
 
-
-
-
-#    __  __      _         __           _      _   _          
-#   |  \/  |__ _(_)_ _    / _|_  _ _ _ | |____| |_(_)___ _ _  
-#   | |\/| / _` | | ' \  |  _| || | ' \| / / _|  _| / _ \ ' \ 
-#   |_|  |_\__,_|_|_||_| |_|  \_,_|_||_|_\_\__|\__|_\___/_||_|
-#  
-
 # Hlavní část skriptu
 if [ -z "$reMarkable_File_ID" ]; then
-    # Pokud je reMarkable_File_ID prázdný zastav script.
     echo "Error: Složka nebyla nalezena."
     exit 1
 else
-    # Pokud je reMarkable_File_ID neprázdný, vyhledej složku podle prefixu
-    latest_directory=$(find_Directory "$reMarkable_Path" "${reMarkable_File_ID}*")
+    # Kontrola, zda je soubor zadán a existuje
+    if [ -z "$specified_file" ]; then
+        echo "Error: Nebyl zadán žádný soubor."
+        exit 1
+    elif [ ! -f "$specified_file" ]; then
+        echo "Error: Soubor '$specified_file' neexistuje."
+        exit 1
+    fi
 
-    # Kopírování souboru do aktuální složky
-    cp "$latest_directory"
+    upgrade_WGET
+
+    # Vyhledání složky odpovídající reMarkable_File_ID
+    matching_directory=$(find_Matching_Directory "$reMarkable_Path" "${reMarkable_File_ID}*")
+
+    if [ -z "$matching_directory" ]; then
+        echo "Error: Složka podle prefixu '$reMarkable_File_ID' nebyla nalezena."
+        exit 1
+    fi
+
+    # Kopírování souboru do nalezené složky
+    cp "$specified_file" "$matching_directory"
+    echo "Info: Soubor '$specified_file' byl úspěšně zkopírován do '$matching_directory'."
 fi
-
-
-
