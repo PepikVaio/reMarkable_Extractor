951f30d7004088a3ed99ee3c6ee99b68b8db0ab7
Wajsar Josef <36114753+PepikVaio@users.noreply.github.com>
2024-08-11 20:35:38 +0200

    Update import.sh

--- import.sh
+++ import.sh
@@ -1,63 +1,29 @@
 #!/bin/bash
 
-#    ___      _   _   _                              __  __          _        _    _     
-#   / __| ___| |_| |_(_)_ _  __ _ ___  ___   _ _ ___|  \/  |__ _ _ _| |____ _| |__| |___ 
-#   \__ \/ -_)  _|  _| | ' \/ _` (_-< |___| | '_/ -_) |\/| / _` | '_| / / _` | '_ \ / -_)
-#   |___/\___|\__|\__|_|_||_\__, /__/       |_| \___|_|  |_\__,_|_| |_\_\__,_|_.__/_\___|
-#                           |___/                                                        
-
-# Zadaný soubor
-specified_file="${1:-}"
+# ID souboru, například eaf3f838
+reMarkable_File_ID="${1:-}"
 
 # Cesta k souborům
-reMarkable_Path="/home/root/.local/share/remarkable/xochitl/d1131219/"
+reMarkable_Path="/home/root/.local/share/remarkable/xochitl/"
 
+# Funkce pro nalezení složky začínající na reMarkable_File_ID a bez tečky na konci
+find_Directory() {
+    local search_path="$1"
+    local prefix="$2"
+    find "$search_path" -maxdepth 1 -type d -name "${prefix}*" ! -name '*.*' -print | head -n 1
+}
 
-WGET="wget"
-
-upgrade_wget() {
-    wget_path=/home/root/.local/share/@Wajsar_Josef/wget
-    wget_remote=http://toltec-dev.org/thirdparty/bin/wget-v1.21.1-1
-    wget_checksum=c258140f059d16d24503c62c1fdf747ca843fe4ba8fcd464a6e6bda8c3bbb6b5
-
-    if [ -f "$wget_path" ] && ! sha256sum -c <(echo "$wget_checksum  $wget_path") > /dev/null 2>&1; then
-        rm "$wget_path"
-    fi
-
-    if ! [ -f "$wget_path" ]; then
-        echo -e "Fetching secure wget..."
-        # Download and compare to hash
-        mkdir -p "$(dirname "$wget_path")"
-        if ! "$WGET" -q "$wget_remote" --output-document "$wget_path"; then
-            echo -e "${COLOR_ERROR}Error: Could not fetch wget, make sure you have a stable Wi-Fi connection${NOCOLOR}"
-            exit 1
-        fi
-    fi
-
-    if ! sha256sum -c <(echo "$wget_checksum  $wget_path") > /dev/null 2>&1; then
-        echo -e "${COLOR_ERROR}Error: Invalid checksum for the local wget binary${NOCOLOR}"
+# Hlavní část skriptu
+if [ -z "$reMarkable_File_ID" ]; then
+    echo "Error: ID souboru není zadáno."
+    exit 1
+else
+    directory=$(find_Directory "$reMarkable_Path" "$reMarkable_File_ID")
+    
+    if [ -n "$directory" ]; then
+        echo "$directory"
+    else
+        echo "Error: Složka podle prefixu '$reMarkable_File_ID' nebyla nalezena."
         exit 1
     fi
-
-    chmod 755 "$wget_path"
-    WGET="$wget_path"
-
-    copy
-}
-
-
-copy() {
-
-    # Přesunutí souboru na reMarkable
-    #$WGET -O "$reMarkable_Path" "$specified_file"
-    
-    # Kopírování souboru do nalezené složky
-    # cp "$specified_file" "$reMarkable_Path"
-    #wget "$specified_file" -O "$reMarkable_Path"
-    scp "$specified_file" "$reMarkable_Path"
-
-
-
-}
-
-upgrade_wget
+fi
