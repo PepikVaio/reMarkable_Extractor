39d59cabbb8092c5be668ee927caf871fb39a981
Wajsar Josef <36114753+PepikVaio@users.noreply.github.com>
2024-08-11 14:20:20 +0200

    Update extract2.sh

--- extract2.sh
+++ extract2.sh
@@ -1,4 +1,11 @@
 #!/bin/bash
 
-file_downloaded=$(./extract.sh)
-echo "Název staženého souboru je: $file_downloaded"
+# Přednastavený název souboru
+file_name="moje_testovaci_soubor.txt"
+
+# Zobrazení názvu souboru
+echo "Název staženého souboru je: $file_name"
+
+# Případná simulace operace, která by použila tento název
+# např. kopírování nebo vytvoření souboru
+# touch "$file_name"
