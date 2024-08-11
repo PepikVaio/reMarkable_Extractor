e50faac9eac249db090c3706eb50f0246e5497cf
Wajsar Josef <36114753+PepikVaio@users.noreply.github.com>
2024-08-11 03:10:31 +0200

    Update extract2.sh

--- extract2.sh
+++ extract2.sh
@@ -1,13 +1,21 @@
 #!/bin/bash
 
-# Zeptejte se uživatele na uživatelské jméno a IP adresu reMarkable zařízení
-read -p "Zadejte uživatelské jméno: " REMOTE_USER
-read -p "Zadejte IP adresu reMarkable zařízení: " REMOTE_IP
+# Přečtěte vstupy ze standardního vstupu (stdin)
+IFS=$'\n' read -d '' -r USERNAME IP_ADDRESS PASSWORD
 
-# Definujte cestu k souboru na reMarkable
+# Odstranění zbytečných mezer
+IP_ADDRESS=$(echo "$IP_ADDRESS" | xargs)
+PASSWORD=$(echo "$PASSWORD" | xargs)
+
+# Zobrazte zadané hodnoty pro kontrolu
+echo "Uživatelské jméno: $USERNAME"
+echo "IP adresa: $IP_ADDRESS"
+echo "Heslo: $PASSWORD"
+
+# Cesta k souboru na reMarkable
 REMOTE_FILE_PATH="/home/root/.local/share/remarkable/xochitl/a05f671f-1b09-4210-bf3c-868879e714b2/7fbc381c-04f8-41c3-a0a3-aa5831d1b64b.rm"
 
 # Stáhněte soubor z reMarkable do aktuální pracovní složky
-scp "${REMOTE_USER}@${REMOTE_IP}:${REMOTE_FILE_PATH}" .
+sshpass -p "$PASSWORD" scp "${USERNAME}@${IP_ADDRESS}:${REMOTE_FILE_PATH}" .
 
 echo "Soubor byl úspěšně stažen do aktuální složky."
