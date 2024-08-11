0dbb0a04a208238fbc82c54f0f030b096a1acf20
Wajsar Josef <36114753+PepikVaio@users.noreply.github.com>
2024-08-11 19:59:46 +0200

    Update import.sh

--- import.sh
+++ import.sh
@@ -12,47 +12,18 @@ specified_file="${1:-}"
 # Cesta k souborům
 reMarkable_Path="/home/root/.local/share/remarkable/xochitl/d1131219/"
 
-WGET="wget"
 
 
-upgrade_wget() {
-    wget_path=/home/root/.local/share/@Wajsar_Josef/wget
-    wget_remote=http://toltec-dev.org/thirdparty/bin/wget-v1.21.1-1
-    wget_checksum=c258140f059d16d24503c62c1fdf747ca843fe4ba8fcd464a6e6bda8c3bbb6b5
 
-    if [ -f "$wget_path" ] && ! sha256sum -c <(echo "$wget_checksum  $wget_path") > /dev/null 2>&1; then
-        rm "$wget_path"
-    fi
 
-    if ! [ -f "$wget_path" ]; then
-        echo -e "Fetching secure wget..."
-        # Download and compare to hash
-        mkdir -p "$(dirname "$wget_path")"
-        if ! "$WGET" -q "$wget_remote" --output-document "$wget_path"; then
-            echo -e "${COLOR_ERROR}Error: Could not fetch wget, make sure you have a stable Wi-Fi connection${NOCOLOR}"
-            exit 1
-        fi
-    fi
-
-    if ! sha256sum -c <(echo "$wget_checksum  $wget_path") > /dev/null 2>&1; then
-        echo -e "${COLOR_ERROR}Error: Invalid checksum for the local wget binary${NOCOLOR}"
-        exit 1
-    fi
-
-    chmod 755 "$wget_path"
-    WGET="$wget_path"
-
-    copy
-}
 
 copy() {
 
     # Přesunutí souboru na reMarkable
-    $WGET -O "$reMarkable_Path" "$specified_file"
+    #$WGET -O "$reMarkable_Path" "$specified_file"
     
     # Kopírování souboru do nalezené složky
-    #cp "$specified_file" "$reMarkable_Path"
+    cp "$specified_file" "$reMarkable_Path"
+
 }
 
-
-upgrade_wget
